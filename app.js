466982a24abe0effbd2a31ef577066e8b07a726f
longdo0612 <longdo061299@gmail.com>
2023-06-04 10:26:02 +0700

    add default endpoint for vercel deployment

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.get('/', (req, res) => {
+  res.send('App is running...');
+});
 app.post('/auth/register', upload.single('picture'), register);
 app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
